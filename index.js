c5c86e5db17bc4ff669eb56cde1f666ab2f4bbcd
TecnoSense23 <gamerprinceytjethwa@gmail.com>
2026-05-24 11:33:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const mineflayer = require('mineflayer');
 const app = express();
 
-// Render को जिंदा रखने के लिए वेब सर्वर
 app.get('/', (req, res) => {
-  res.send('Jarvis Bot is Alive!');
+  res.send('Jarvis is Alive!');
 });
 
 app.listen(3000, () => {
@@ -23,37 +22,7 @@ function startBot() {
     console.log('Jarvis has spawned successfully!');
   });
 
-  bot.on('chat', (username, message) => {
-    if (username === bot.username) return;
-    bot.chat('Hello! I am an AFK Bot.');
-  });
-
-  // ऑटो-रिस्पॉन
-  bot.on('death', () => {
-    console.log('Jarvis mar gaya! Auto-respawning...');
-    bot.respawn();
-  });
-
-  // अटरनोस को चकमा देने के लिए प्रो AFK मूवमेंट
-  setInterval(() => {
-    if (bot.entity) {
-      bot.swingArm('right'); 
-      bot.setControlState('sneak', true); 
-      setTimeout(() => {
-        if (bot.entity) bot.setControlState('sneak', false); 
-      }, 500); 
-      bot.look(Math.random() * Math.PI * 2, 0); 
-    }
-  }, 10000); 
-
-  // एंटी-एएफके ऑटो जंप
-  bot.on('physicTick', () => {
-    if (bot.entity) {
-      bot.setControlState('jump', true);
-    }
-  });
-
-  bot.on('disconnect', () => {
+  bot.on('disconnect', (err) => {
     console.log('Bot disconnected. Reconnecting in 10 seconds...');
     setTimeout(startBot, 10000);
   });
@@ -63,5 +32,4 @@ function startBot() {
   });
 }
 
-// बॉट स्टार्ट करें
 startBot();
